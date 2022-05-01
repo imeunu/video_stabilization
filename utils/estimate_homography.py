2a88d998b5cb6b22c50c436c8f7846e21cc13b6f
imeunu <imeunukr@gmail.com>
2022-05-01 23:48:00 +0900

    .

--- utils/estimate_homography.py
+++ utils/estimate_homography.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 
-def get_toy():
+def get_toy(n=0):
     '''Assign frame pairs to variables'''
     root = '/Users/eunu/Desktop/code/video_stabilization/toy/consecutive'
     # adjust the root variable with your environment
@@ -16,7 +16,7 @@ def get_toy():
         #         stable.append(img)
         #     else:
         #         unstable.append(img)
-        img = cv2.imread(f'{root}/{i}/0000.jpg')
+        img = cv2.imread(f'{root}/{i}/000{n}.jpg')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         if i == 'stable': stable = img
         else: unstable = img
@@ -39,46 +39,53 @@ def find_H(f1, f2):
     src = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape((-1,1,2))
     dst = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape((-1,1,2))
     H, status = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+
+    # Scale the matrix with median eigenvalue
+    _, v, _ = np.linalg.svd(H)
+    H = H / np.median(v)
     return (H)
 
-def find_t(f1, f2):
-    '''Extract affine translation parameters from H'''
-    H = find_H(f1, f2)
-    return (H[0][2], H[1][2])
+def compute_t(H):
+    
 
-def find_camera_distance(stable, unstable):
-    '''Find x,y distance between camera center'''
-    t = [find_t(stable[i], unstable[i]) for i in range(len(stable))]
-    return (np.mean(t, axis=0))
+# def find_t(f1, f2):
+#     '''Extract affine translation parameters from H'''
+#     H = find_H(f1, f2)
+#     return (H[0][2], H[1][2])
 
-def decompose_H(H):
-    '''Decompose homography matrix by SVD and find matrices R, t'''
-    u, s, vh = np.linalg.svd(np.dot(H,H.transpose()))
-    v1, _, v3 = (u + vh) / 2
-    l1, _, l3 = sorted(np.sqrt(s), reverse=True)
-    z1, z3 = get_z(l1, l3)
-    v_1, v_3 = get_v(z1, v1, l1, l3), get_v(z3, v3, l1, l3)
-    t, n = get_t_n(v_1, v_3, z1, z3)
-    Itnt = np.identity(3) + np.outer(t.transpose(), n)
-    R = np.matmul(H, np.linalg.inv(Itnt))
-    return (R, t)
+# def find_camera_distance(stable, unstable):
+#     '''Find x,y distance between camera center'''
+#     t = [find_t(stable[i], unstable[i]) for i in range(len(stable))]
+#     return (np.mean(t, axis=0))
 
-def get_z(l1, l2):
-    root = np.sqrt(1 + 4 * l1 * l2 / (l1 - l2) ** 2)
-    z1, z2 = (-1 + root) / (2 * l1 * l2), (-1 - root) / (2 * l1 * l2)
-    return (z1, z2)
+# def decompose_H(H):
+#     '''Decompose homography matrix by SVD and find matrices R, t'''
+#     u, s, vh = np.linalg.svd(np.dot(H,H.transpose()))
+#     v1, _, v3 = (u + vh) / 2
+#     l1, _, l3 = sorted(np.sqrt(s), reverse=True)
+#     z1, z3 = get_z(l1, l3)
+#     v_1, v_3 = get_v(z1, v1, l1, l3), get_v(z3, v3, l1, l3)
+#     t, n = get_t_n(v_1, v_3, z1, z3)
+#     Itnt = np.identity(3) + np.outer(t.transpose(), n)
+#     R = np.matmul(H, np.linalg.inv(Itnt))
+#     return (R, t)
 
-def get_v(zeta, v, l1, l2):
-    v_norm = (zeta ** 2) * ((l1 - l2) ** 2) + 2 * zeta * (l1 * l2 - 1) + 1
-    norm = np.sqrt(v_norm)
-    return (norm * v)
+# def get_z(l1, l2):
+#     root = np.sqrt(1 + 4 * l1 * l2 / (l1 - l2) ** 2)
+#     z1, z2 = (-1 + root) / (2 * l1 * l2), (-1 - root) / (2 * l1 * l2)
+#     return (z1, z2)
 
-def get_t_n(v_1, v_3, z1, z3):
-    # t, n can be either positive or negative
-    # Also, it can be v_1 - v_3
-    t = v_1 + v_3 / (z1 - z3)
-    n = (z1 * v_3 + z3 * v_1) / (z1 - z3)
-    return (t, n)
+# def get_v(zeta, v, l1, l2):
+#     v_norm = (zeta ** 2) * ((l1 - l2) ** 2) + 2 * zeta * (l1 * l2 - 1) + 1
+#     norm = np.sqrt(v_norm)
+#     return (norm * v)
+
+# def get_t_n(v_1, v_3, z1, z3):
+#     # t, n can be either positive or negative
+#     # Also, it can be v_1 - v_3
+#     t = v_1 + v_3 / (z1 - z3)
+#     n = (z1 * v_3 + z3 * v_1) / (z1 - z3)
+#     return (t, n)
 
 def new_solution(H):
     S = np.matmul(H.transpose(), H)
@@ -89,10 +96,6 @@ def mat_minor(M,m,n):
     M = np.delete(M,m-1,0)
     M = np.delete(M,n-1,1)
     return (np.linalg.det(M))
-
-def sign(x):
-    if x >= 0: return 1
-    else: return -1
 
 if __name__ == '__main__':
     stable, unstable = get_toy()
